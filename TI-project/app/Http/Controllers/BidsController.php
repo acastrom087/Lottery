b712b659130e113c644f084cb7c49ffb1bbf996f
czunigar <chala926@gmail.com>
2021-08-16 03:48:09 -0600

    bid limit functional

--- TI-project/app/Http/Controllers/BidsController.php
+++ TI-project/app/Http/Controllers/BidsController.php
@@ -52,10 +52,10 @@ class BidsController extends Controller
         $lottery = Lottery::find($request->lottery_id);
         
         $newBalance = $request->bid + $lottery->balance;
-        //Bid::create($request->all());
+        Bid::create($request->all());
 
         $bids = Bid::select('bid')
-                    ->where('lottery_id', $request->lottery_id)
+                    ->where('lottery_id', $lottery->id)
                     ->orderByRaw('bid DESC')
                     ->get();
 
@@ -64,57 +64,62 @@ class BidsController extends Controller
         $bottomBid = 0;
         
         if ($bids != null) {
-            if(!$bids[0] == null){
-                $topBid = $bids[0]->bid * 60;
+            $top3 = collect([]);
+            for ($i=0; $i < $bids->count(); $i++) { 
+                $top3[$i] = $bids[$i]->bid;
             }
-            if(!$bids[1] == null){
-                $topBid = $bids[0]->bid * 60;
-                $midBid = $bids[1]->bid * 10;
+
+            if($top3->count() == 1){
+                $topBid = $top3[0] * 60;
             }
-            if(!$bids[2] == null){
-                $topBid = $bids[0]->bid * 60;
-                $midBid = $bids[1]->bid * 10;
-                $bottomBid = $bids[2]->bid * 5;
+            elseif($top3->count() == 2){
+                $topBid = $top3[0] * 60;
+                $midBid = $top3[1] * 10;
+            }
+            elseif($top3->count() >= 3){
+                $topBid = $top3[0] * 60;
+                $midBid = $top3[1] * 10;
+                $bottomBid = $top3[2] * 5;
             }
         }
 
         $apuestaTotal = $topBid + $midBid + $bottomBid;
         $limite = $newBalance - $apuestaTotal;
         
-        return $apuestaTotal; 
         
-        /*if($limite >= 0){
+        if($limite >= 0){
             $lottery->update(['balance' => $newBalance]);
-            return back()->with('success','Bid Creado');
+            return back()->with('success','Bid made');
         }
         elseif($limite < 0){
             $bid = Bid::all();
             $eliminar = $bid->last();
             Bid::destroy($eliminar->id);
 
-            if($bids->count() < 1){
-                $limit = (($limite / 60) * 0.0165) + $limite / 60;
+            $bids2 = Bid::select('bid')
+                            ->where('lottery_id', $lottery->id)
+                            ->orderByRaw('bid DESC')
+                            ->get();
+
+            $limit = 0;
+
+            if($bids2->count() == 0){
+                $limit = ($newBalance / 60) - 5;
             }
-            elseif($bids->count() == 1){
-                $limit = ((($limTopBid + $limite) / 10) * 0.055) + (($limTopBid + $limite) / 10);
+            elseif($bids2->count() == 1){
+                $limit = (($newBalance - $topBid) / 10) - 5;
             }
-            elseif($bids->count() == 2){
-                $limit = ((($limTopBid + $limMidBid + $limite) / 5) * 0.115) + (($limTopBid + $limMidBid + $limite) / 5);
+            elseif($bids2->count() == 2){
+                $limit = (($newBalance - $topBid - $midBid)  / 5) - 5;
             }
-            elseif($bids->count() > 3){
-                $limit = ($bottomBid / 5) - 1;
+            elseif($bids2->count() >=  3){
+                $bottom = $bids2[2]->bid;
+                $limit = ($bottom) - 1;
             }
 
-            return back()->with('success','Bid has reached the limit available, please make a bid less than $'.$limit);
-        }*/
-        
-
-        
-
-        
+            return back()->with('success','Limit reached, please make a bid equal or less than $'.$limit);
+        }
         
     }
 
-    
-  
 }
